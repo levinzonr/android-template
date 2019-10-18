5388edd31d7fbe2bb28cc90edc3a8fea7590935b
Roman Levinzon <levinzon.roman@gmail.com>
2019-10-18 18:20:52 +0200

    Init template

---
+++ app/src/main/java/cz/levinzonr/spotistats/injection/modules/RestRepositoryBinding.kt
@@ -0,0 +1,13 @@
+package cz.levinzonr.spotistats.injection.modules
+
+import cz.levinzonr.spotistats.network.RestPostRepository
+import cz.levinzonr.spotistats.repositories.PostRepository
+import org.koin.dsl.module
+
+
+val repositoryModule = module {
+
+    single<PostRepository> { RestPostRepository(get()) }
+
+
+}
